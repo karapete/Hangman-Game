2d9e313181f251edec50d56a06e9182a0cc70d80
Kara Peterson <karapeterson@Karas-MacBook.local>
2017-07-01 23:58:25 -0600

    first commit

---
+++ game.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+		document.getElementById("initialOutput").innerHTML = "Press any key to get started!";
+        document.onkeyup = function(initialButton){
+          document.getElementById("initialOutput").innerHTML = "Let's Begin!";
+        }
